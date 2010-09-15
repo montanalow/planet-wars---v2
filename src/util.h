945b1c83fed61c478d1f954e54e3a48ec851eb78
montana <montana@10.0.1.200>
2010-09-14 23:27:54 -0700

    use cerr for logging instead to keep things simple

--- src/util.h
+++ src/util.h
@@ -28,43 +28,5 @@ public:
   static std::vector<std::string> tokenize(const std::string& s, const std::string& delimiters = std::string(" "));
 };
 
-#ifdef LOGGING_ENABLED
-class logger {
-public:
-  static std::ofstream log;
-};
-#else /* LOGGING_ENABLED */
-class dead_end {
-public:
-  dead_end& operator<<(bool&){return *this;}
-  dead_end& operator<<(short&){return *this;}
-  dead_end& operator<<(unsigned short&){return *this;}
-  dead_end& operator<<(int&){return *this;}
-  dead_end& operator<<(unsigned int&){return *this;}
-  dead_end& operator<<(long&){return *this;}
-  dead_end& operator<<(unsigned long&){return *this;}
-  dead_end& operator<<(float&){return *this;}
-  dead_end& operator<<(double&){return *this;}
-  dead_end& operator<<(long double&){return *this;}
-  dead_end& operator<<(const void*){return *this;}
-  dead_end& operator<<(std::streambuf*){return *this;}
-  dead_end& operator<<(std::ostream&){return *this;}
-  dead_end& operator<<(std::ios&){return *this;}
-  dead_end& operator<<(std::ios_base&){return *this;}
-  dead_end& operator<<(char){return *this;}
-  dead_end& operator<<(signed char){return *this;}
-  dead_end& operator<<(unsigned char){return *this;}
-  dead_end& operator<<(const char*){return *this;}
-  dead_end& operator<<(const signed char*){return *this;}
-  dead_end& operator<<(const unsigned char*){return *this;}
-  void flush(){}
-};
-
-class logger {
-public:
-  static dead_end log;
-};
-#endif /* LOGGING_ENABLED */
-
 }
 #endif	/* UTIL_H */
