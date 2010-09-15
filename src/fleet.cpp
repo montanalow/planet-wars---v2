961b109cfb55fbaff3a344a1b66492ea0d3a33d6
montana <montana@10.0.1.200>
2010-09-14 20:16:36 -0700

    add indexes for allied and enemy fleets and planets

--- src/fleet.cpp
+++ src/fleet.cpp
@@ -2,9 +2,6 @@
 
 pw::fleet::fleet(int owner, int ships, pw::planet& source, pw::planet& destination, int total_trip_time, int time_remaining, pw::game_state& game_state) :
   _owner(owner), _ships(ships), _source(source), _destination(destination), _total_trip_time(total_trip_time), _time_remaining(time_remaining), _game_state(game_state) {
-  if (_time_remaining > _game_state._max_fleet_time_remaining) {
-    _game_state._max_fleet_time_remaining = _time_remaining;
-  }
 }
 
 int pw::fleet::owner() const {
@@ -47,9 +44,6 @@ void pw::fleet::game_state(const pw::game_state& game_state) {
   _game_state = game_state;
   _source = _game_state.planets()[_source.id()];
   _destination = _game_state.planets()[_destination.id()];
-  if (_time_remaining > _game_state._max_fleet_time_remaining) {
-    _game_state._max_fleet_time_remaining = _time_remaining;
-  }
 }
 
 const pw::fleet& pw::fleet::operator=(const pw::fleet& fleet) {
